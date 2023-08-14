c4a66401daf318209c2f22c06a1f013cbca9a7b7
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-08-14 14:44:36 +0200

    complete components

--- client-user-management/src/app/app-routing.module.ts
+++ client-user-management/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserComponent } from './components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path:'', redirectTo:'login', pathMatch:'full'},
+   {path:'login', component:LoginComponent},
+   {path:'register', component:RegisterComponent},
+   {path:'profile', component:ProfileComponent},
+   {path:'user-list', component:UserListComponent},
+   {path:'user', component:UserComponent},
+   {path:'user/:id', component:UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client-user-management/src/app/components/user/user.component.ts
+++ client-user-management/src/app/components/user/user.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from "../../model/user";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+
+  userId: string;
+  currentUser: User;
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.currentUser = JSON.parse(localStorage.getItem("detailUser"));
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      if (params.has('id')) {
+        this.userId = params.get('id');
+      }
+    });
+  }
 
 }
